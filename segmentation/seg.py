e21d7d7a4f417f5b0be0bdb4710def1333441261
Ahmed-Ashraf <ahmed.ashraf@ibm.com>
2022-09-02 19:42:01 +0200

    Blurring, Thresholding images

--- segmentation/seg.py
+++ segmentation/seg.py
@@ -1,24 +1,20 @@
+import cv2
 class CharacterDetection:
-    def __init__(self, imgplate):
-        self.licensePlate = imgplate
+    def __init__(self, LocalizedImagePath):
+        self.ImagePath = LocalizedImagePath
         print("Segmentation Constructor")
-    #def GaussianBlur(self,path):
-     #   image = cv2.imread(path)
-        # apply guassian blur on The image
-      #  blurred = cv2.GaussianBlur(image, (5, 5), cv2.BORDER_DEFAULT)
-        # display Blurred image
-       # cv2.imwrite(path, blurred)
-        # cv2.waitKey(0)  # waits until a key is pressed
-        #print("Blurred")
-        #return path
-    #def Adaptive_Threshold(self,path):
-     #   image = cv2.imread(path)
-      #  image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-       # threshed = cv2.adaptiveThreshold(image, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 199, 5)
-        #cv2.imwrite(path, threshed)
-        #  print("Threshedddd")
-        # cv2.waitKey(0)
-        #return path
-    def masking(self,img):
+
+    def GaussianBlur(self):
+        image = cv2.imread(self.ImagePath)
+        blurred = cv2.GaussianBlur(image, (5, 5), cv2.BORDER_DEFAULT)
+        cv2.imwrite(self.ImagePath, blurred)
+
+    def Adaptive_Threshold(self):
+        image = cv2.imread(self.ImagePath)
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        threshed = cv2.adaptiveThreshold(image, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 199, 5)
+        cv2.imwrite(self.ImagePath, threshed)
+
+    def masking(self):
         print("masking func")
 
