af474b44f0752cccf5d7a72950b279c756be4732
Ziad Muhammed <Ziad.Muhammed@ibm.com>
2022-09-11 17:01:29 +0200

    feat: integration segmentation with localization

---
+++ src/loc.py
@@ -0,0 +1,9 @@
+class Localization:
+    def __init__(self):
+        print("localization constructor")
+
+    def crop(self):
+        print("crop")
+
+    def recognizePlate(self):
+        print("rp")
