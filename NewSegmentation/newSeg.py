45a1f81918e36b23b91be2621242a6299e2105f3
Ziad Muhammed <Ziad.Muhammed@ibm.com>
2022-09-14 00:34:47 +0200

    feat:new segmentation

---
+++ NewSegmentation/newSeg.py
@@ -0,0 +1,91 @@
+import cv2
+import os
+import numpy as np
+from matplotlib import pyplot as plt
+
+path = './outputs'
+j = 1
+
+for filename in os.scandir("IBM/detections"):
+    gray = cv2.imread(filename.path, 0)
+    gray = cv2.resize(gray, (200, 100))
+    gray = cv2.resize(gray, None, fx=3, fy=3, interpolation=cv2.INTER_CUBIC)
+    blur = cv2.GaussianBlur(gray, (5, 5), 0)
+    # gray = cv2.medianBlur(gray, 3)
+    ret, thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_OTSU | cv2.THRESH_BINARY_INV)
+
+    rect_kern = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
+    # apply dilation
+    dilation = cv2.dilate(thresh, rect_kern, iterations=1)
+    # find contours
+    try:
+        contours, hierarchy = cv2.findContours(dilation, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    except:
+        ret_img, contours, hierarchy = cv2.findContours(dilation, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    Contourss = sorted(contours, key=cv2.contourArea, reverse=True)[:10]
+    # create copy of image
+    im2 = gray.copy()
+    plate_num = ""
+    imageResult = []
+    listOfContours = []
+    # loop through contours and find letters in license plate
+    for cnt in Contourss:
+        x, y, w, h = cv2.boundingRect(cnt)
+        height, width = im2.shape
+        # if height of box is not a quarter of total height then skip
+        if float(h) < height / 6:
+            continue
+        ratio = h / float(w)
+        # if height to width ratio is less than 1.5 skip
+        area = h * w
+        # if width is not more than 25 pixels skip
+        if float(w) > width / 7:
+            continue
+        # if area is less than 100 pixels skip
+        if area < 1000:
+            continue
+        if ratio > 7.5:
+            continue
+        listOfContours.append(x)
+        # print(len(listOfContours))
+
+        charCopy = np.zeros((44, 24))
+        # draw the rectangle
+        char = thresh[y - 5:y + h + 5, x - 5:x + w + 5]
+        try:
+            char = cv2.resize(char, (20, 40))
+        except Exception:
+            continue
+
+        cv2.rectangle(im2, (x - 5, y - 40), (x + w + 5, y + h + 18), (0, 255, 0), 2)
+        char = cv2.bitwise_not(char)
+
+
+        charCopy[2:42, 2:22] = char
+        charCopy[0:2, :] = 0
+        charCopy[:, 0:2] = 0
+        charCopy[42:44, :] = 0
+        charCopy[:, 22:24] = 0
+        imageResult.append(charCopy)
+
+    indices = sorted(range(len(listOfContours)), key=lambda k: listOfContours[k])
+    imageResultCopy = []
+    print(len(imageResult))
+    print(len(indices))
+    for index in indices:
+        if index < len(imageResult):
+            imageResultCopy.append(imageResult[index])  # stores character images according to their index
+    imageResult = np.array(imageResultCopy)
+    imageResultCopy.clear()
+    ### for testing and printing
+    # print(len(listOfContours))
+    # for i in range(len(imageResult)):
+    #     plt.subplot(1, 10, i + 1)
+    #     plt.imshow(imageResult[i], cmap='gray')
+    #     cv2.imwrite(f'{path}{j}.png', imageResult[i])
+    #     j += 1
+    #     plt.axis('off')
+    # plt.show()
+    # cv2.imshow("Character's Segmented", im2)
+    # cv2.waitKey(0)
+    # cv2.destroyAllWindows()
--- main.py
+++ main.py
@@ -1,10 +1,9 @@
-from distutils.log import error
 import os
 from collections import namedtuple
 
 from matplotlib import pyplot as plt
 from recognition.rec import Recognition
-
+from integeration.merge import endPoint
 from skimage.filters import threshold_local
 from skimage import segmentation
 from skimage import measure
@@ -17,7 +16,7 @@ import sys
 path = './outputs/'
 sys.path.insert(0, './localisation')
 import detect as detect
-from segmentation.seg import Segmentation
+# from segmentation.seg import Segmentation
 from absl import app
 
 
@@ -33,9 +32,8 @@ def predict(image_path):
     crop_path = detect.crop_one(image_path)[0]
 
 
-
-#def main(_argv):
- #   predict("./localisation/data/images/IMG_5351.JPEG")
+# def main(_argv):
+#   predict("./localisation/data/images/IMG_5351.JPEG")
 
 
 def printChars(chars, segObj, count):
@@ -48,24 +46,26 @@ def printChars(chars, segObj, count):
         plt.axis('off')
     plt.show()
 
+
 if __name__ == '__main__':
     try:
         # app.run(main)
-        countPlate = 1
-        predict("./localisation/data/images/IMG_5482.JPEG")
-        rec = Recognition('vgg_model')
-        for filename in os.scandir(".\detections"):
-            try:
-                segObject = Segmentation(filename.path)
-                chars = segObject.run()
-                printChars(chars, segObject, countPlate)
-                countPlate += 1
-            except Exception:
-                print("error")
-                continue
-        for filename in os.scandir("./outputs/1"):
-            rec.test_data([filename.path])
-            #rec.test_data(['./outputs/1/1.png', './outputs/1/2.png',
-            #            './outputs/1/3.png', './outputs/1/4.png', './outputs/1/5.png', './outputs/1/6.png'])
+        # countPlate = 1
+        # # predict("./localisation/data/images/IMG_5482.JPEG")
+        # rec = Recognition('vgg_model')
+        # for filename in os.scandir(".\detections"):
+        #     try:
+        #         segObject = Segmentation(filename.path)
+        #         chars = segObject.run()
+        #         printChars(chars, segObject, countPlate)
+        #         countPlate += 1
+        #     except Exception:
+        #         print("error")
+        #         continue
+        # for filename in os.scandir("./outputs/1"):
+        #     rec.test_data([filename.path])
+        endPoint('123ABC')
+        # rec.test_data(['./outputs/1/1.png', './outputs/1/2.png',
+        #            './outputs/1/3.png', './outputs/1/4.png', './outputs/1/5.png', './outputs/1/6.png'])
     except SystemExit:
         pass
