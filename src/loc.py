e7de3b2e71471c6ddf7f5292aadde4597c971144
Ziad Muhammed <Ziad.Muhammed@ibm.com>
2022-09-18 15:41:13 +0200

    refactor:delete src

--- src/loc.py
+++
@@ -1,9 +0,0 @@
-class Localization:
-    def __init__(self):
-        print("localization constructor")
-
-    def crop(self):
-        print("crop")
-
-    def recognizePlate(self):
-        print("rp")
